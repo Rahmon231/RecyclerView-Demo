70884ef1b3ba6bce48aad2728c8137429c376e25
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-10 01:55:44 +0100

    card recycler view

---
+++ app/src/main/java/com/lemzeeyyy/recyclerviewdemo/card_recycler_view/CardRecyclerView.java
@@ -0,0 +1,14 @@
+package com.lemzeeyyy.recyclerviewdemo.card_recycler_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_recycler_view);
+    }
+}

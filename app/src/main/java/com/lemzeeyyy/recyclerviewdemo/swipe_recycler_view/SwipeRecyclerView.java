6408eab00714eef5218697f5549fb97154db38b5
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-11 23:50:04 +0100

    swipe recycler view

---
+++ app/src/main/java/com/lemzeeyyy/recyclerviewdemo/swipe_recycler_view/SwipeRecyclerView.java
@@ -0,0 +1,14 @@
+package com.lemzeeyyy.recyclerviewdemo.swipe_recycler_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SwipeRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_recycler_view);
+    }
+}
